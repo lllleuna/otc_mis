fbe9bb63e3333d65f3b586bf6a1f7464d25cf153
Leunamme <leuna.atutubo@gmail.com>
2025-03-22 09:39:02 +0800

    switcher

--- resources/views/components/page-switcher.blade.php
+++ resources/views/components/page-switcher.blade.php
@@ -1,6 +1,6 @@
 {{-- page-switcher.blade.php --}}
 
-<div class="fixed bottom-4 right-4 z-50">
+<div class="fixed bottom-4 right-4 z-100">
     <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-1.5 flex items-center">
         @php
             $currentUrl = request()->url();
