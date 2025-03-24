b81b98076d0e12b981073a17b71b162b12269f11
Leunamme <leuna.atutubo@gmail.com>
2025-03-24 23:32:59 +0800

    fixed success message

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,21 +5,28 @@
     <div class="mx-4 relative overflow-x-auto shadow-md sm:rounded-lg">
         <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
             <div>
-                <select class="block p-2 mx-5 text-sm text-gray-900 border border-gray-300 rounded-lg w-40 bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
-                    <option>All</option>
-                    <option>Approver</option>
-                    <option>Encoder</option>
-                    <option>Viewer</option>
-                </select>
+                @if (session('success'))
+                    <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4">
+                        {{ session('success') }}
+                    </div>
+                @endif
             </div>
 
             <div class="flex items-center">
                 <div class="relative">
-                    <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                        <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                    <div
+                        class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                clip-rule="evenodd"></path>
+                        </svg>
                     </div>
                     <form action="/search" method="GET">
-                        <input type="text" name="search" id="table-search" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search for user">
+                        <input type="text" name="search" id="table-search"
+                            class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                            placeholder="Search for user">
                         <button type="submit" class="hidden">Search</button>
                     </form>
                 </div>
@@ -32,7 +39,8 @@
                 <tr>
                     <th scope="col" class="p-4">
                         <div class="flex items-center">
-                            <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
+                            <input id="checkbox-all-search" type="checkbox"
+                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
                             <label for="checkbox-all-search" class="sr-only">checkbox</label>
                         </div>
                     </th>
@@ -45,24 +53,27 @@
             </thead>
             <tbody>
                 @foreach ($users as $user)
-                <tr class="bg-white border-b hover:bg-gray-50">
-                    <td class="w-4 p-4">
-                        <div class="flex items-center">
-                            <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
-                            <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                        </div>
-                    </td>
-                    <td class="px-6 py-4">
-                        <a class="font-medium text-gray-700">{{ $user['firstname'] . " " . $user['lastname'] }}</a><br>
-                        <a class="">{{ $user['email'] }}</a>
-                    </td>
-                    <td class="px-6 py-4">{{ $user['employee_id_no'] }}</td>
-                    <td class="px-6 py-4">{{ $user['division'] }}</td>
-                    <td class="px-6 py-4">{{ $user['role'] }}</td>
-                    <td class="px-6 py-4">
-                        <a href="/users/{{ $user->id }}" class="font-medium text-blue-600 hover:underline">See more</a>
-                    </td>
-                </tr>
+                    <tr class="bg-white border-b hover:bg-gray-50">
+                        <td class="w-4 p-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-table-search-1" type="checkbox"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
+                                <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a
+                                class="font-medium text-gray-700">{{ $user['firstname'] . ' ' . $user['lastname'] }}</a><br>
+                            <a class="">{{ $user['email'] }}</a>
+                        </td>
+                        <td class="px-6 py-4">{{ $user['employee_id_no'] }}</td>
+                        <td class="px-6 py-4">{{ $user['division'] }}</td>
+                        <td class="px-6 py-4">{{ $user['role'] }}</td>
+                        <td class="px-6 py-4">
+                            <a href="/users/{{ $user->id }}" class="font-medium text-blue-600 hover:underline">See
+                                more</a>
+                        </td>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
