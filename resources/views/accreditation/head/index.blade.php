20ce4974f854b22d8b1504c2cd1330e382fe65f5
Leunamme <leuna.atutubo@gmail.com>
2025-03-25 10:48:05 +0800

    type

--- resources/views/accreditation/head/index.blade.php
+++ resources/views/accreditation/head/index.blade.php
@@ -109,7 +109,7 @@
                                         {{ $application->region }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 region-name">
-                                        {{ $application->application_type }}
+                                        {{ ucwords($application->application_type) }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         @php
