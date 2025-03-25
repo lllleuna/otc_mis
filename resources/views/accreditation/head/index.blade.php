4baf026faa24055b7ff78ad5be37bfcf34e46579
Leunamme <leuna.atutubo@gmail.com>
2025-03-25 10:46:30 +0800

    type

--- resources/views/accreditation/head/index.blade.php
+++ resources/views/accreditation/head/index.blade.php
@@ -88,7 +88,7 @@
                                     Region</th>
                                 <th
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b">
-                                    Application Type</th>
+                                    Type</th>
                                 <th
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b">
                                     Status</th>
