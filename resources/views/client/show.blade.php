505c695e407b162ef5a7b37b09a7ff40ebde13cc
Leunamme <leuna.atutubo@gmail.com>
2025-04-01 01:38:03 +0800

    layout

--- resources/views/client/show.blade.php
+++ resources/views/client/show.blade.php
@@ -25,9 +25,9 @@
                             @endphp
                             <div class="flex items-center">
                                 <span
-                                    class="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium {{ $isActive ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800' }}">
+                                    class="inline-flex items-center px-5 py-2 rounded-full text-sm font-medium {{ $isActive ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800' }}">
                                     <span
-                                        class="h-2 w-2 mr-2 rounded-full {{ $isActive ? 'bg-green-500' : 'bg-red-500' }}"></span>
+                                        class="h-2 w-2 rounded-full {{ $isActive ? 'bg-green-500' : 'bg-red-500' }}"></span>
                                     {{ $isActive ? 'Active' : 'Not Active' }}
                                 </span>
                             </div>
