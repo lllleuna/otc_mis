4432b5299427e804741d76d054df92c111852496
Leunamme <leuna.atutubo@gmail.com>
2025-04-01 02:57:57 +0800

    head

--- app/Exports/AccreditationReportExport.php
+++ app/Exports/AccreditationReportExport.php
@@ -4,15 +4,16 @@ namespace App\Exports;
 
 use App\Models\GeneralInfo;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings; // Import WithHeadings concern
 
-class AccreditationReportExport implements FromCollection
+class AccreditationReportExport implements FromCollection, WithHeadings
 {
     /**
      * @return \Illuminate\Support\Collection
      */
     public function collection()
     {
-        // Assuming you're using the same query as the controller
+        // Fetch the data you want to export
         return GeneralInfo::selectRaw("
                 cda_registration_no, 
                 accreditation_no, 
@@ -24,5 +25,23 @@ class AccreditationReportExport implements FromCollection
             ")
             ->whereNotNull('accreditation_no')
             ->get();
+    }
+
+    /**
+     * Define the column headers for the export
+     *
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'CDA Registration No',   // Header for the first column
+            'Accreditation No',      // Header for the second column
+            'Name',                  // Header for the third column
+            'Region',                // Header for the fourth column
+            'City',                  // Header for the fifth column
+            'Status',                // Header for the sixth column
+            'Accreditation Date',    // Header for the seventh column
+        ];
     }
 }
