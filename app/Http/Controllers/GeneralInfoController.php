41579e918bad06ce5adc7892381a7ed8c94a7561
Leunamme <leuna.atutubo@gmail.com>
2025-03-31 14:54:38 +0800

    fix

--- app/Http/Controllers/GeneralInfoController.php
+++ app/Http/Controllers/GeneralInfoController.php
@@ -9,18 +9,18 @@ class GeneralInfoController extends Controller
 {
     public function index()
     {
-        // Get the latest records grouped by accreditation_no
         $generalInfos = GeneralInfo::select('accreditation_no', 'accreditation_date', 'region', 'city', 'email', 'contact_no')
             ->whereIn('id', function ($query) {
                 $query->selectRaw('MAX(id)')
-                      ->from('general_info')
-                      ->groupBy('accreditation_no');
+                    ->from('general_info')
+                    ->groupBy('accreditation_no');
             })
             ->orderBy('accreditation_date', 'desc')
             ->get();
-
-        return view('client.index', compact('generalInfos'));
+    
+        return view('general-info.index', compact('generalInfos'));
     }
+    
 
     public function show($accreditation_no)
     {
