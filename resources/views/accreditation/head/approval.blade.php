40295a370f09c8ab6b34eaf1e214096cb1782f85
Leunamme <leuna.atutubo@gmail.com>
2025-04-06 10:22:14 +0800

    button to top

--- resources/views/accreditation/head/approval.blade.php
+++ resources/views/accreditation/head/approval.blade.php
@@ -60,4 +60,10 @@
             </button>
         </form>
     </div>
+
+    <button id="backToTopBtn"
+        class="hidden fixed bottom-8 right-8 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full shadow-lg transition-opacity duration-300">
+        ↑ Top
+    </button>
+
 </x-layout>
