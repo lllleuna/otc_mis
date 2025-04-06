5762e6fa4184d7f2402f814b06777c1de07ef82e
Leunamme <leuna.atutubo@gmail.com>
2025-04-06 09:52:15 +0800

    px

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -15,7 +15,7 @@
             <div class="flex items-center gap-4">
                 {{-- Division Dropdown --}}
                 <select id="divisionFilter"
-                    class="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
+                    class="block p-2 text-sm px-5 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
                     <option value="">All Divisions</option>
                     <option value="OD">OD</option>
                     <option value="PED">PED</option>
