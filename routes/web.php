555dcdc8cecd6d113a00a2aa5b5d0f42a079a5a2
Leunamme <leuna.atutubo@gmail.com>
2025-04-19 00:55:19 +0800

    email verify route fixed

--- routes/web.php
+++ routes/web.php
@@ -80,7 +80,7 @@ Route::get('/email/verify', function () {
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
 
-    return route('dashboard');
+    return redirect()->route('dashboard');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
