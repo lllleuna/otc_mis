f669135efdc7e30cbdc05fe59a1af28668504661
Leunamme <leuna.atutubo@gmail.com>
2025-04-19 01:36:04 +0800

    empty fixed

--- resources/views/components/evaluationInfo.blade.php
+++ resources/views/components/evaluationInfo.blade.php
@@ -193,232 +193,243 @@
     </div>
 </div>
 
-
-<div class="my-10">
-    <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
-        Units
-    </h3>
-
-    <table class="w-full text-left border-collapse text-sm">
-        <thead class="text-gray-400">
-            <tr class="bg-gray-100">
-                <th class="px-4 py-2">Type of Unit</th>
-                <th class="px-4 py-2">Cooperatively Owned</th>
-                <th class="px-4 py-2">Individually Owned</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($appUnits as $appUnit)
-                <tr class="hover:bg-gray-50">
-                    <td class="px-4 py-2">{{ $appUnit->type_of_unit ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $appUnit->cooperatively_owned ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $appUnit->individually_owned ?? 'N/A' }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-
-</div>
-
-<div class="my-10">
-    <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
-        Franchise
-    </h3>
-
-    <table class="w-full text-left border-collapse text-sm">
-        <thead class="text-gray-400">
-            <tr class="bg-gray-100">
-                <th class="px-4 py-2">Route</th>
-                <th class="px-4 py-2">CPC Case No</th>
-                <th class="px-4 py-2">Type of Unit</th>
-                <th class="px-4 py-2">Validity</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($appFranchises as $franchise)
-                <tr class="hover:bg-gray-50">
-                    <td class="px-4 py-2">{{ $franchise->route ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $franchise->cpc_case_number ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $franchise->type_of_unit ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $franchise->validity ?? 'N/A' }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-</div>
-
-
-
-<div class="my-10">
-    <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
-        Loans
-    </h3>
-
-    <table class="w-full text-left border-collapse text-sm">
-        <thead class="text-gray-400">
-            <tr class="bg-gray-100">
-                <th class="px-4 py-2">Financing Institution</th>
-                <th class="px-4 py-2">Date Acquired</th>
-                <th class="px-4 py-2">Amount</th>
-                <th class="px-4 py-2">Utilization</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($appLoans as $loan)
-                <tr class="hover:bg-gray-50">
-                    <td class="px-4 py-2">{{ $loan->financing_institution ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $loan->acquired_at ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $loan->amount ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $loan->utilization ?? 'N/A' }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-</div>
-
-<div class="my-10">
-    <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
-        Business
-    </h3>
-    <table class="w-full text-left border-collapse text-sm">
-        <thead class="text-gray-400">
-            <tr class="bg-gray-100">
-                <th class="px-4 py-2">Type</th>
-                <th class="px-4 py-2">Nature of Business</th>
-                <th class="px-4 py-2">Starting Capital</th>
-                <th class="px-4 py-2">Capital to Date</th>
-                <th class="px-4 py-2">Years of Existence</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($appBusinesses as $business)
-                <tr class="hover:bg-gray-50">
-                    <td class="px-4 py-2">{{ $business->type ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $business->nature_of_business ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $business->starting_capital ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $business->capital_to_date ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $business->years_of_existence ?? 'N/A' }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-</div>
-
-<div class="my-10">
-    <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
-        Trainings/Seminar
-    </h3>
-    <table class="w-full text-left border-collapse text-sm">
-        <thead class="text-gray-400">
-            <tr class="bg-gray-100">
-                <th class="px-4 py-2">Title of Training</th>
-                <th class="px-4 py-2">No of Attendees</th>
-                <th class="px-4 py-2">Total Fund</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($appTrainings as $training)
-                <tr class="hover:bg-gray-50">
-                    <td class="px-4 py-2">{{ $training->title_of_training ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $training->no_of_attendees ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $training->total_fund ?? 'N/A' }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-</div>
-
-
-
-<div class="my-10">
-    <h3 class=" mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
-        Membership Details
-    </h3>
-
-    <div class="flex justify-between pt-5">
+@if (!empty($appUnits) && $appUnits->count())
+    <div class="my-10">
+        <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
+            Units
+        </h3>
 
         <table class="w-full text-left border-collapse text-sm">
             <thead class="text-gray-400">
                 <tr class="bg-gray-100">
-                    <th class="px-4 py-2">Total Members</th>
-                    <th class="px-4 py-2">SSS Enrolled</th>
-                    <th class="px-4 py-2">Pag-IBIG Enrolled</th>
-                    <th class="px-4 py-2">PhilHealth Enrolled</th>
-                    <th class="px-4 py-2">With CETOS</th>
-                    <th class="px-4 py-2">Without CETOS</th>
+                    <th class="px-4 py-2">Type of Unit</th>
+                    <th class="px-4 py-2">Cooperatively Owned</th>
+                    <th class="px-4 py-2">Individually Owned</th>
                 </tr>
             </thead>
             <tbody>
-                <tr class="hover:bg-gray-50">
-                    <td class="px-4 py-2">{{ $appCetos->total ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $appGenInfo->sss_enrolled ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $appGenInfo->pagibig_enrolled ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $appGenInfo->philhealth_enrolled ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $appCetos->members_with ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $appCetos->members_without ?? 'N/A' }}</td>
-                </tr>
+                @foreach ($appUnits as $appUnit)
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-4 py-2">{{ $appUnit->type_of_unit ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $appUnit->cooperatively_owned ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $appUnit->individually_owned ?? 'N/A' }}</td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
 
     </div>
+@endif
 
-</div>
+@if (!empty($appFranchises) && $appFranchises->count())
+    <div class="my-10">
+        <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
+            Franchise
+        </h3>
 
-<div class="my-10">
-    <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
-        Governance
-    </h3>
-
-    <table class="w-full text-left border-collapse text-sm">
-        <thead class="text-gray-400">
-            <tr class="bg-gray-100">
-                <th class="px-4 py-2">Role</th>
-                <th class="px-4 py-2">Full Name</th>
-                <th class="px-4 py-2">Term</th>
-                <th class="px-4 py-2">Contact</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($appGov as $gov)
-                <tr class="hover:bg-gray-50">
-                    <td class="px-4 py-2">{{ $gov->role_name ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $gov->first_name ?? 'N/A' }} {{ $gov->middle_name ?? 'N/A' }}
-                        {{ $gov->last_name ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $gov->term_start ?? 'N/A' }} - {{ $gov->term_end ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $gov->mobile_number ?? 'N/A' }} <br> {{ $gov->email ?? 'N/A' }}</td>
+        <table class="w-full text-left border-collapse text-sm">
+            <thead class="text-gray-400">
+                <tr class="bg-gray-100">
+                    <th class="px-4 py-2">Route</th>
+                    <th class="px-4 py-2">CPC Case No</th>
+                    <th class="px-4 py-2">Type of Unit</th>
+                    <th class="px-4 py-2">Validity</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($appFranchises as $franchise)
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-4 py-2">{{ $franchise->route ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $franchise->cpc_case_number ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $franchise->type_of_unit ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $franchise->validity ?? 'N/A' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-</div>
+    </div>
+@endif
 
-<div class="my-10">
-    <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
-        Awards
-    </h3>
-    <table class="w-full text-left border-collapse text-sm">
-        <thead class="text-gray-400">
-            <tr class="bg-gray-100">
-                <th class="px-4 py-2">Awarding Body</th>
-                <th class="px-4 py-2">Nature of Award</th>
-                <th class="px-4 py-2">Date Received</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($appAwards as $award)
-                <tr class="hover:bg-gray-50">
-                    <td class="px-4 py-2">{{ $award->awarding_body ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $award->nature_of_award ?? 'N/A' }}</td>
-                    <td class="px-4 py-2">{{ $award->date_received ?? 'N/A' }}</td>
+@if (!empty($appLoans) && $appLoans->count())
+    <div class="my-10">
+        <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
+            Loans
+        </h3>
+
+        <table class="w-full text-left border-collapse text-sm">
+            <thead class="text-gray-400">
+                <tr class="bg-gray-100">
+                    <th class="px-4 py-2">Financing Institution</th>
+                    <th class="px-4 py-2">Date Acquired</th>
+                    <th class="px-4 py-2">Amount</th>
+                    <th class="px-4 py-2">Utilization</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($appLoans as $loan)
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-4 py-2">{{ $loan->financing_institution ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $loan->acquired_at ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $loan->amount ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $loan->utilization ?? 'N/A' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-</div>
+    </div>
+@endif
+
+@if (!empty($appBusinesses) && $appBusinesses->count())
+    <div class="my-10">
+        <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
+            Business
+        </h3>
+        <table class="w-full text-left border-collapse text-sm">
+            <thead class="text-gray-400">
+                <tr class="bg-gray-100">
+                    <th class="px-4 py-2">Type</th>
+                    <th class="px-4 py-2">Nature of Business</th>
+                    <th class="px-4 py-2">Starting Capital</th>
+                    <th class="px-4 py-2">Capital to Date</th>
+                    <th class="px-4 py-2">Years of Existence</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($appBusinesses as $business)
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-4 py-2">{{ $business->type ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $business->nature_of_business ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $business->starting_capital ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $business->capital_to_date ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $business->years_of_existence ?? 'N/A' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+@endif
+
+@if (!empty($appTrainings) && $appTrainings->count())
+    <div class="my-10">
+        <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
+            Trainings/Seminar
+        </h3>
+        <table class="w-full text-left border-collapse text-sm">
+            <thead class="text-gray-400">
+                <tr class="bg-gray-100">
+                    <th class="px-4 py-2">Title of Training</th>
+                    <th class="px-4 py-2">No of Attendees</th>
+                    <th class="px-4 py-2">Total Fund</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($appTrainings as $training)
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-4 py-2">{{ $training->title_of_training ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $training->no_of_attendees ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $training->total_fund ?? 'N/A' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+@endif
+
+
+@if (!empty($appCetos))
+    <div class="my-10">
+        <h3 class=" mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
+            Membership Details
+        </h3>
+
+        <div class="flex justify-between pt-5">
+
+            <table class="w-full text-left border-collapse text-sm">
+                <thead class="text-gray-400">
+                    <tr class="bg-gray-100">
+                        <th class="px-4 py-2">Total Members</th>
+                        <th class="px-4 py-2">SSS Enrolled</th>
+                        <th class="px-4 py-2">Pag-IBIG Enrolled</th>
+                        <th class="px-4 py-2">PhilHealth Enrolled</th>
+                        <th class="px-4 py-2">With CETOS</th>
+                        <th class="px-4 py-2">Without CETOS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-4 py-2">{{ $appCetos->total ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $appGenInfo->sss_enrolled ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $appGenInfo->pagibig_enrolled ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $appGenInfo->philhealth_enrolled ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $appCetos->members_with ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $appCetos->members_without ?? 'N/A' }}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+
+    </div>
+@endif
+
+@if (!empty($appGov) && $appGov->count())
+    <div class="my-10">
+        <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
+            Governance
+        </h3>
+
+        <table class="w-full text-left border-collapse text-sm">
+            <thead class="text-gray-400">
+                <tr class="bg-gray-100">
+                    <th class="px-4 py-2">Role</th>
+                    <th class="px-4 py-2">Full Name</th>
+                    <th class="px-4 py-2">Term</th>
+                    <th class="px-4 py-2">Contact</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($appGov as $gov)
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-4 py-2">{{ $gov->role_name ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $gov->first_name ?? 'N/A' }} {{ $gov->middle_name ?? 'N/A' }}
+                            {{ $gov->last_name ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $gov->term_start ?? 'N/A' }} - {{ $gov->term_end ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $gov->mobile_number ?? 'N/A' }} <br> {{ $gov->email ?? 'N/A' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+@endif
+
+@if (!empty($appAwards) && $appAwards->count())
+    <div class="my-10">
+        <h3 class="mt-10 text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
+            Awards
+        </h3>
+        <table class="w-full text-left border-collapse text-sm">
+            <thead class="text-gray-400">
+                <tr class="bg-gray-100">
+                    <th class="px-4 py-2">Awarding Body</th>
+                    <th class="px-4 py-2">Nature of Award</th>
+                    <th class="px-4 py-2">Date Received</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($appAwards as $award)
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-4 py-2">{{ $award->awarding_body ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $award->nature_of_award ?? 'N/A' }}</td>
+                        <td class="px-4 py-2">{{ $award->date_received ?? 'N/A' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+@endif
