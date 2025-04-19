c245a11a3a2a191756cbe44e1f13c4747acedef5
Leunamme <leuna.atutubo@gmail.com>
2025-04-20 01:07:48 +0800

    debugging

--- resources/views/accreditation/officer/index.blade.php
+++ resources/views/accreditation/officer/index.blade.php
@@ -145,12 +145,12 @@
                                         @elseif ($application->status === 'evaluated' && Auth::user()->role === 'Admin')
                                             <a href="{{ route('accreditation.approval', $application->id) }}"
                                                 class="text-blue-600 hover:text-blue-900 bg-blue-50 hover:bg-blue-100 px-3 py-1 rounded-md transition-colors">
-                                                View
+                                                View1
                                             </a>
                                         @elseif ($application->status === 'evaluated' && Auth::user()->role === 'Officer 1' || Auth::user()->role === 'Officer 2')
                                             <a href="{{ route('applications.history', $application->id) }}"
                                                 class="text-blue-600 hover:text-blue-900 bg-blue-50 hover:bg-blue-100 px-3 py-1 rounded-md transition-colors">
-                                                View
+                                                View2
                                             </a>
                                         @elseif ($application->status === 'approved')
                                             <a href="{{ route('accreditation.release', $application->id) }}"
@@ -160,7 +160,7 @@
                                         @elseif ($application->status === 'released' || $application->status === 'rejected')
                                             <a href="{{ route('applications.history', $application->id) }}"
                                                 class="text-blue-600 hover:text-blue-900 bg-blue-50 hover:bg-blue-100 px-3 py-1 rounded-md transition-colors">
-                                                View
+                                                View3
                                             </a>
                                         @else
                                             <span class="text-gray-500">N/A</span>
