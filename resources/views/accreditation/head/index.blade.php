5166178e3b673f331cd38fcd8d9f2b383e98bd69
Leunamme <leuna.atutubo@gmail.com>
2025-04-28 01:54:56 +0800

    hide data

--- resources/views/accreditation/head/index.blade.php
+++ resources/views/accreditation/head/index.blade.php
@@ -78,16 +78,28 @@
                 </div>
             @endif
 
-            @if ($applications->isEmpty())
+            @if (!request('search'))
                 <div class="bg-gray-50 rounded-lg p-8 text-center">
                     <svg class="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <h3 class="mt-2 text-lg font-medium text-gray-900">No applications found</h3>
+                    <h3 class="mt-2 text-lg font-medium text-gray-900">Start Your Search</h3>
                     <p class="mt-1 text-sm text-gray-500">
-                        {{ request('search') ? 'No results matching your search criteria.' : 'There are no applications in this category yet.' }}
+                        Use the search bar above to find accreditation applications by Reference Number.
+                    </p>
+                </div>
+            @elseif ($applications->isEmpty())
+                <div class="bg-gray-50 rounded-lg p-8 text-center">
+                    <svg class="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <h3 class="mt-2 text-lg font-medium text-gray-900">No Applications Found</h3>
+                    <p class="mt-1 text-sm text-gray-500">
+                        We couldn't find any applications matching your search criteria.
                     </p>
                 </div>
             @else
